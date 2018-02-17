f314da80795541d5b0a5adfaa4d0e5680d866aa4
gerasysti <36553078+gerasysti@users.noreply.github.com>
2018-02-16 22:33:52 -0200

    Upload inicial

---
+++ dpr/Remun_eSocial.dpr
@@ -0,0 +1,44 @@
+program Remun_eSocial;
+
+uses
+  Forms,
+  Principal in '..\pas\Principal.pas' {frmPrincipal},
+  VarGlobais in '..\pas\VarGlobais.pas',
+  UtilsDb in '..\pas\UtilsDb.pas',
+  Padrao1 in '..\pas\Padrao1.pas' {frmPadrao1},
+  CadTabPadrao2 in '..\pas\CadTabPadrao2.pas' {frmCadTabPadrao2},
+  udmPrincipal in '..\pas\udmPrincipal.pas' {dmPrincipal: TDataModule},
+  uEditComandosSQL in '..\pas\uEditComandosSQL.pas' {frmEditComandosSQL},
+  gsLib in '..\pas\gsLib.pas',
+  uSobre in '..\pas\uSobre.pas' {frmSobre},
+  uLogin in '..\pas\uLogin.pas' {frmLogin},
+  CadPadrao in '..\pas\CadPadrao.pas' {frmCadPadrao},
+  AddEditPadrao in '..\pas\AddEditPadrao.pas' {frmAddEditPadrao},
+  AddEditPadrao2 in '..\pas\AddEditPadrao2.pas' {frmAddEditPadrao2},
+  AlteraSenha in '..\pas\AlteraSenha.pas' {frmAlteraSenha},
+  uESocial in '..\pas\uESocial.pas',
+  VareSocial in 'VareSocial.pas',
+  SelecionaMesAno in '..\pas\SelecionaMesAno.pas' {frmSelecionaMesAno};
+
+{$R *.res}
+
+begin
+
+  Application.Initialize;
+
+  Application.Title := 'REMUNERATU$ - e-Social...';
+  Application.CreateForm(TdmPrincipal, dmPrincipal);
+  Application.CreateForm(TfrmPrincipal, frmPrincipal);
+  Application.CreateForm(TfrmSobre, frmSobre);
+  Application.CreateForm(TfrmEditComandosSQL, frmEditComandosSQL);
+  Application.CreateForm(TfrmSobre, frmSobre);
+  Application.CreateForm(TfrmLogin, frmLogin);
+  Application.CreateForm(TfrmAlteraSenha, frmAlteraSenha);
+  Application.CreateForm(TfrmSelecionaMesAno, frmSelecionaMesAno);
+  frmSobre.Show;
+  frmSobre.Update;
+
+  frmSobre.VerificaVersao('N');
+  Application.Run;
+
+end.
---
+++ dpr/VareSocial.pas
@@ -0,0 +1,54 @@
+unit VareSocial;
+
+interface
+
+var
+   glb_1000id,
+   glb_1000pAmb,
+   glb_1000TpProcEmi,
+   glb_1000glb_1000VerProc,
+   glb_1000TpInsc,
+   glb_1000NrInscr,
+   glb_1000ModoLancamento,
+   glb_1000IniValid,
+   glb_1000FimValid,
+   glb_1000NmRazao,
+   glb_1000ClassTrib,
+   glb_1000NatJurid,
+   glb_1000IndCoop,
+   glb_1000IndConstr,
+   glb_1000IndDesFolha,
+   glb_1000IndOptRegEletron,
+   glb_1000IndEtt,
+   glb_1000nrSiafi,
+   glb_1000nmEnte,
+   glb_1000uf,
+   glb_1000vrSubteto,
+   glb_1000IdeMinLei,
+   glb_1000NrCertif,
+   glb_1000DtEmisCertif,
+   glb_1000DtVencCertif,
+   glb_1000NrProtRenov,
+   glb_1000DtProtRenov,
+   glb_1000DtDou,
+   glb_1000PagDou,
+   glb_1000NmCtt,
+   glb_1000CpfCtt,
+   glb_1000FoneFixo,
+   glb_1000FoneCel,
+   glb_1000Email,
+   glb_1000IndAcordoIsenMulta,
+   glb_1000SHCnpjSoftHouse,
+   glb_1000SHNmRazao,
+   glb_1000SHNmCont,
+   glb_1000SHTelefone,
+   glb_1000SHemail,
+   glb_1000IndSitPJ,
+   glb_1000IndSitPF,
+   glb_1000NovaIniValid,
+   glb_1000NovaFimValid:String;
+
+
+implementation
+
+end.
